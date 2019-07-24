0b291dc27fda7566cf2a0246327849d0aea2eeaf
jiay <amande@appledeMacBook-Air.local>
2019-07-24 21:10:22 +0800

    test

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import '@svgdotjs/svg.draggable.js';
 import Container from './elem/container';
 
 const draw = SVG().addTo('#app').size(900, 900);
-const container = new Container(draw, 300, 300);
+const container = new Container(draw, 300, 300)
